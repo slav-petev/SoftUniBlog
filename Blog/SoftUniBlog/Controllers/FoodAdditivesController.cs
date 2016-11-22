b84eea67550479c1e4c1b85d41df60fa8e571ddd
Doroteya <d.y.doncheva@hotmail.com>
2016-11-22 18:36:26 +0200

    Added Food Additives lol

---
+++ Blog/SoftUniBlog/Controllers/FoodAdditivesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftUniBlog.Controllers
+{
+    public class FoodAdditivesController : Controller
+    {
+        // GET: FoodAdditives
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
